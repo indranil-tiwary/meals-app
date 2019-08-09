6bbfaf055065380e9feb26a2ca1002d1818afe9f
Indranil Tiwary <indraniltiwary@Indranils-MacBook-Pro.local>
2019-08-09 15:48:17 +0530

    Init Folder Structure commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../data/dummy-data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routename = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
